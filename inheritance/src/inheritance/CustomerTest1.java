97a501cb0105534162f453d29e210a7ce411bb6f
Lee Jong Soo <jongsu2645@gmail.com>
2021-07-21 19:15:51 +0900

    Study Inheritance

---
+++ inheritance/src/inheritance/CustomerTest1.java
@@ -0,0 +1,17 @@
+package inheritance;
+
+public class CustomerTest1 {
+    public static void main(String[] args){
+        Customer customerLee = new Customer();
+        customerLee.setCustomerID(10010);
+        customerLee.setCustomerName("Lee Jongsu");
+        customerLee.bonusPoint = 1000;
+        System.out.println(customerLee.showCustomerInfo());
+
+        VIPCustomer customerKim = new VIPCustomer();
+        customerKim.setCustomerID(10020);
+        customerKim.setCustomerName("Kim JJap");
+        customerKim.bonusPoint = 1000;
+        System.out.println(customerKim.showCustomerInfo());
+    }
+}
