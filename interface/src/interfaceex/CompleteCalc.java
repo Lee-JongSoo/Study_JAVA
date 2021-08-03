1aac24b61dedbba412b720e1f0c363c1ec063aa7
Lee Jong Soo <jongsu2645@gmail.com>
2021-08-03 18:26:29 +0900

    Study Interface

---
+++ interface/src/interfaceex/CompleteCalc.java
@@ -0,0 +1,24 @@
+package interfaceex;
+
+public class CompleteCalc extends Calculator {
+	@Override
+	public int times(int num1, int num2) {
+		return num1*num2;
+	}
+	
+	@Override
+	public int divide(int num1, int num2) {
+		if(num2 != 0)
+			return num1/num2;
+		else
+			return Calc.ERROR;
+	}
+	
+	public int square(int num) {
+		return num*num;
+	}
+	
+	public void showInfo() {
+		System.out.println("Calc interface");
+	}
+}
