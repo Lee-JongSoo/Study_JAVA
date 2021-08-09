158f5a5d3e8b2f5aadd227603d6a7f0be3959990
Lee Jong Soo <jongsu2645@gmail.com>
2021-08-09 15:59:05 +0900

    Study Object Class in equals() method

---
+++ object/src/object/EqualsTest.java
@@ -0,0 +1,58 @@
+package object;
+
+class Student{
+    int studentID;
+    String studentName;
+
+    public Student(int studentID, String studentName) {
+        this.studentID = studentID;
+        this.studentName = studentName;
+    }
+
+    public String toString(){
+        return studentID + ", " + studentName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Student){
+            Student std = (Student)obj;
+            if(this.studentID == std.studentID)
+                return true;
+            else return false;
+        }
+        return false;
+    }
+}
+
+public class EqualsTest {
+    public static void main(String[] args){
+        Student studentLee = new Student(100, "Lee JongSu");
+        Student studentLee2 = studentLee;
+        Student studentSang = new Student(100, "Lee JongSu");
+
+        if (studentLee == studentLee2){
+            System.out.println("same");
+        }else {
+            System.out.println("different");
+        }
+
+        if (studentLee.equals(studentLee2)){
+            System.out.println("same");
+        }else {
+            System.out.println("different");
+        }
+
+        if (studentLee == studentSang){
+            System.out.println("same");
+        }else {
+            System.out.println("different");
+        }
+
+        if (studentLee.equals(studentSang)){
+            System.out.println("same");
+        }else {
+            System.out.println("different");
+        }
+    }
+}
